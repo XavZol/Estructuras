42072983f388c12d9fe2b0a60dbaa7b91b2c2a7a
XavZol <alansolis707@gmail.com>
2025-08-18 02:33:17 -0600

    Modificacion de programa para solicitar datos

--- ProgEstruc.cpp
+++ ProgEstruc.cpp
@@ -1,3 +1,4 @@
+//Solicita datos de la persona en la estructura
 // Iniciamos con estructuras de datos en C++
 #include<iostream>
 #include<conio.h>  
@@ -6,16 +7,16 @@ using namespace std;
 struct persona{
 char nombre[20];
 int edad;
-}
-persona2 = {"Alan", 27}, 
-persona3 = {"Juan", 30};
-int main (){
-cout<<"Nombre de la persona 2: "<<persona2.nombre<<endl;
-cout<<"Edad de la persona 2: "<<persona2.edad<<endl;
+}persona1;
+int main(){
+cout<< "Ingrese el nombre de la persona: ";
+cin.getline(persona1.nombre, 20, '\n');
+cout<< "Ingrese la edad de la persona: ";
+cin>>persona1.edad;
 
-cout<<"Nombre de la persona 3; "<<persona3.nombre<<endl;
-cout<<"Edad de la persona 3: "<<persona3.edad<<endl;
-
+ cout<< "\nDatos de la persona:\n";
+cout<< "Nombre: " << persona1.nombre << endl;
+cout<< "Edad: " << persona1.edad << endl;   
 getch();
 return 0;
 }
