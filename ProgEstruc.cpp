4bb250ca20e0119b0d652a2710cdc9fe54e75cd8
XavZol <alansolis707@gmail.com>
2025-08-20 06:32:56 -0600

    Estructuras anidadas

--- ProgEstruc.cpp
+++ ProgEstruc.cpp
@@ -1,23 +1,42 @@
-//Solicita datos de la persona en la estructura
-// Iniciamos con estructuras de datos en C++
 #include<iostream>
-#include<conio.h>  
+#include<conio.h>
 using namespace std;
-
-struct persona{
-char nombre[20];
-int edad;
-}persona1;
+struct info_direccion{
+    char direccion[30];
+    char ciudad[20];
+    char provincia[20];
+};
+struct empleado{
+    char nombre[20];
+    struct info_direccion dir_empleado;
+    double salario;
+}empleados[2];
+// double sirve para almacenar el salario del empleado haciendo uso de la estructura info_direccion
 int main(){
-cout<< "Ingrese el nombre de la persona: ";
-cin.getline(persona1.nombre, 20, '\n');
-cout<< "Ingrese la edad de la persona: ";
-cin>>persona1.edad;
-
- cout<< "\nDatos de la persona:\n";
-cout<< "Nombre: " << persona1.nombre << endl;
-cout<< "Edad: " << persona1.edad << endl;   
-getch();
-return 0;
+for( int i=0; i<2; i++){
+    fflush(stdin); // Limpiar el buffer de entrada
+    cout<<"Digite su nombre: ";
+    cin.getline(empleados[i].nombre,20,'\n');
+    cout<<"Digite su direccion: ";
+    cin.getline(empleados[i].dir_empleado.direccion,30,'\n');
+    cout<<"Digite su ciudad: ";
+    cin.getline(empleados[i].dir_empleado.ciudad,20,'\n');
+    cout<<"Digite su provincia: ";
+    cin.getline(empleados[i].dir_empleado.provincia,20,'\n');
+    cout<<"Digite su salario: ";
+    cin>>empleados[i].salario;
+    cout<<"\n";
+   //cin.ignore(); // Limpiar el buffer de entrada
 }
-
+//imprimir los datos de los empleados
+for(int i=0;i<2;i++){
+    cout<<"Nombre: "<<empleados[i].nombre<<endl;
+    cout<<"Direccion: "<<empleados[i].dir_empleado.direccion<<endl;
+    cout<<"Ciudad: "<<empleados[i].dir_empleado.ciudad<<endl;
+    cout<<"Provincia: "<<empleados[i].dir_empleado.provincia<<endl;
+    cout<<"Salario: "<<empleados[i].salario<<endl;
+    cout<<"\n";
+}
+    getch ();
+    return 0;
+}
