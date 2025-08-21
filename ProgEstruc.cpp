3a274d0ece2863c25af662f65f554160a5032aed
XavZol <alansolis707@gmail.com>
2025-08-21 01:20:35 -0600

    Programa de estructuras de quien tiene mejor promedio de alumnos

--- ProgEstruc.cpp
+++ ProgEstruc.cpp
@@ -1,43 +1,38 @@
+/* Hacer una estructura llamada alumno, en la cual se tendrán los siguientes Campos:
+Nombre, edad, promedio, pedir datos al usuario para 3 alumnos, comprobar cual de los 3 tiene el mejor promedio y posteriormente imprimir los datos del alumno.
+*/
 #include<iostream>
 #include<conio.h>
-#include<string.h>
 using namespace std;
-/* Hacer una estructura llamada corredor, en la cual se tendran los siguientes campos: Nombre, edad, sexo, club; y pedir datos al usuario para un corredor. Ademaás asignarle una categoría de competición:
- - Juvenil <= 18 años
- - Señor <= 40 años
- - Veterano <= 40 años
- Posteriormente imprimir todos los datos del corredor, incluida su categoría de competición*/
-struct Corredor{
-    char Nombre[20];
-    int Edad;
-    char Sexo [10];
-    char Club [20];
-} Corr1;
- int main(){
-char categoria[20];
-    cout<<"Nombre: "<<endl;
-    cin.getline(Corr1.Nombre,20,'\n');
-    cout<<"Edad: ";
-    cin>>Corr1.Edad;
-    fflush(stdin); // vaciar el baffle
-    cout<<"Sexo: ";
-    cin.getline(Corr1.Sexo,10,'\n');
-    cout<<"Club: ";
-    cin.getline(Corr1.Club,20,'\n');
 
-    if(Corr1.Edad <= 18){
-        strcpy(categoria,"Juvenil");
-    } else if(Corr1.Edad <=40){
-        strcpy(categoria,"Adulto");
-    } else {
-        strcpy(categoria,"Veterano");
+struct Alumno{
+char  nombre [20];
+int edad;
+float promedio;
+}alumnos[3]; 
+
+int main(){
+float mayor=0;
+int pos;
+// Guardando o almacenando los datos para 3 alumnos.
+for(int i=0;i<3;i++){
+    fflush(stdin); // vaciar el baffle
+    cin.ignore(); //vaciar el baffle
+    cout<<i+1<<". Digite su nombre: "; cin.getline(alumnos[i].nombre,20,'\n');
+    cout<<i+1<<". Digite su edad: "; cin>>alumnos[i].edad;
+    cout<<i+1<<". Digite su promedio: "; cin>>alumnos[i].promedio;
+    cout<<"'\n";
+// Comprobando cual de los 3 alumnos tiene el mejor promedio.
+    if(alumnos[i].promedio>mayor){
+        mayor = alumnos[i].promedio;
+        pos = i; 
     }
-  cout<<"\n\nDatos del corredor: \n";
-  cout<<"Nombre del corredor: "<<Corr1.Nombre<<endl;
-  cout<<"Edad del corredor: "<<Corr1.Edad<<endl;
-  cout<<"Sexo del corredor: "<<Corr1.Sexo<<endl;
-  cout<<"Club asignado para el corredor: "<<Corr1.Club<<endl;
-  cout<<"Categoria asignada al corredor: "<<categoria;
+}
+// Imprimiendo quien tiene le mejor promedio.
+cout<<"El alumno que mejor promedio tiene es \n";
+cout<<"\nNombre: "<<alumnos[pos].nombre<<endl;
+cout<<"Edad: "<<alumnos[pos].edad<<endl;
+cout<<"Promedio: "<<alumnos[pos].promedio<<endl;
 getch();
 return 0;
- }
+}
