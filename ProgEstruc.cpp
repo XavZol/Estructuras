6a2e0d26d156d50c039b8c799341460a702fbc14
XavZol <alansolis707@gmail.com>
2025-08-23 16:22:39 -0600

    Mismo programa de promedio pero solicitando la cantidad de alumnos

--- ProgEstruc.cpp
+++ ProgEstruc.cpp
@@ -1,4 +1,4 @@
-/* Hacer 2 estructuras una llamada promedio que tendrá los siguientes campos:
+/* Hacer N estructuras una llamada promedio que tendrá los siguientes campos:
 nota1, nota2, nota3; y otro llamada alumno que tendrá los siguientes miembros:
 nombre, sexo, edad; hacer que la estructura promedio este anidada en la estructura
 alumno, luego pedir todos los datos para un alummno, luego calcular su promedio, y por 
@@ -7,7 +7,6 @@ ultimo imprimir todos sus datos incluidos el promedio*/
 #include<iostream>
 #include<conio.h>
 using namespace std;
-
 struct promedio{
     float nota1;
     float nota2;
@@ -18,28 +17,37 @@ struct Alumno{
     char sexo[10];
     int edad;
     struct promedio prom;
-}alumno1;
+}alumno[100];
 int main(){
-float promedio_alumno;
-    cout<<"Digite su nombre: ";
-    cin.getline(alumno1.nombre,20,'\n');
-    cout<<"Digite su sexo: ";
-    cin.getline(alumno1.sexo,10,'\n');
-    cout<<"Digite su edad: ";
-    cin>>alumno1.edad;
+int numero_alumnos, posM;
+float promedio_alumno[100], mayor=0;
+cout<<"Digite el numero de alumnos: ";
+cin>>numero_alumnos;
 
-    cout<<"\n.: Notas del Alumno: \n";
-    cout<<"Nota1: "; cin>>alumno1.prom.nota1;
-    cout<<"Nota2: "; cin>>alumno1.prom.nota2;
-    cout<<"Nota3: "; cin>>alumno1.prom.nota3;
+for(int i=0; i<numero_alumnos; i++){
+    cin.ignore();
+    cout<<i+1<<". Digite su nombre: "; cin.getline(alumno[i].nombre,20,'\n');
+    cout<<i+1<<". Digite su sexo: "; cin.getline(alumno[i].sexo,10,'\n');
+    cout<<i+1<<". Digite su edad: "; cin>>alumno[i].edad;
 
-    promedio_alumno = (alumno1.prom.nota1+alumno1.prom.nota2+alumno1.prom.nota3)/3;
+cout<<"\nSolicitamos las notas: \n";
+cout<<"Digite la primera nota: "; cin>>alumno[i].prom.nota1;
+cout<<"Digite la segunda nota: "; cin>>alumno[i].prom.nota2;
+cout<<"Digite la tercer nota: "; cin>>alumno[i].prom.nota3;
+//obtener el promedio del alumno
+promedio_alumno[i] = (alumno[i].prom.nota1+alumno[i].prom.nota2+alumno[i].prom.nota3)/3;
 
-cout<<"\n Datos del alumno\n";
-cout<<"Nombre: "<<alumno1.nombre<<endl;
-cout<<"Sexo: "<<alumno1.sexo<<endl;
-cout<<"Edad: "<<alumno1.edad<<endl;
-cout<<"El promedio del alumno: "<<promedio_alumno<<endl;
+if(promedio_alumno[i] > mayor ){
+    mayor = promedio_alumno[i];
+    posM = i;
+}
+}
+cout<<"\n Mostrando datos del alumno con el mejor promedio\n"
+cout<<"Nombre: "<<alumno[posM].nombre<<endl;
+cout<<"Sexo: "<<alumno[posM].sexo<<endl;
+cout<<"Edad: "<<alumno[posM].edad<<endl;
+cout<<"Promedio: "<<promedio_alumno[posM]<<endl;
+
 getch();
 return 0;
 }
