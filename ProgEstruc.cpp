b112af7a03943c08ed7b5fc3fb2701611a8b6c22
unknown <alansolis707@gmail.com>
2025-10-17 22:21:16 -0600

    Estructuras programa de personas discapacitadas

--- ProgEstruc.cpp
+++ ProgEstruc.cpp
@@ -1,41 +1,46 @@
-/*Defina una estructura que indique el tiempo empleaod por un ciclista en una etapa, La estructura debe tener tres campos: horas
-minutos y segundos, Escriba un programa que dado n etapas calcule el tiempo total empleado en correr todas las etapas.*/
+/*defina una estructura que sirva para representar a una personas. La estructura debe contener dos campos
+: el nombre de la persona y un valor de tipo lógico que indica si la persona tiene algún tipo de discapacidad
+Realice un programa que dado un vector de personas rellene dos nuevos vectores: uno que contenga las personas que no tienen
+ninguna discapacidad y otro que contenga las personas con discapacidad.*/
 #include<iostream>
 #include<conio.h>
+#include<string.h>
 using namespace std;
-struct Etapas{
-
-    int horas;
-    int minutos;
-    int segundos;
-
-}et[100];
-
+struct Persona{
+    char nombre [30];
+    bool discapacidad;
+}personas[100], personasSinD[100], personasConD[100];
 int main(){
-int n_etapas, horasT=0, minutosT=0, segundosT=0;
-cout<<"Digite el numero de etapas: "; cin>>n_etapas;
-
-for(int i=0;i<n_etapas;i++){
-    cout<<"Horas: "; cin>>et[i].horas;
-    cout<<"Minutos: "; cin>>et[i].minutos;
-    cout<<"segundos: "; cin>>et[i].segundos;
-horasT = horasT + et[i].horas;
-minutosT = minutosT+ et[i].minutos;
-if(minutosT >= 60){
-    minutosT -= 60;
-    horasT++;
+    int numero_personas, j=0, k=0;
+    fflush(stdin);
+    cin.ignore();
+    cout<<"Digite el numero de personas:";
+    cin>>numero_personas;
+    for(int i=0; i<numero_personas; i++){
+        cout<<"Digite el nom[bre de la persona:";
+        cin.getline(personas[i].nombre,30,'\n');
+        cout<<"La personas tiene alguna discapacidad (1/0):";
+        cin>>personas[i].discapacidad;
+        
+//almacenar personas con discapacidad y sin discapacidad 
+if (personas[i].discapacidad == 1){
+    strcpy(personasConD[j].nombre, personas[i].nombre);
+    j++;
+}else{ 
+    strcpy(personasSinD[k].nombre, personas[i].nombre);
+    k++;
 }
-segundosT = segundosT + et[i].segundos;
-if(segundosT >= 60){
-    segundosT -= 60;
-    minutosT++;
-}
-
-cout<<"\n Tiempo total empleado en toda la carrera es \n";
-cout<<"Horas: "<<horasT<<endl;
-cout<<"Minutos: "<<minutosT<<endl;
-cout<<"Segundos: "<<segundosT<<endl;
-}
-getch();
+    }
+    cout<<"\nPersonas sin discapacidad:\n";
+    for(int i=0; i<k; i++){
+        cout<<personasSinD[i].nombre<<endl;
+    }
+    cout<<"\nPersonas con discapacidad:\n";
+    for(int i=0; i<j; i++){
+        cout<<personasConD[i].nombre<<endl;
+    }
+//flush para limpiar el bufer y también otro comando
+    getch();
     return 0;
 }
+//bool es para definir valores lógicos 
